8f875652f70c635098633bbcf30c0d6392b33dc7
Kumi <gyabaahkelvin27@gmail.com>
2026-01-31 23:58:32 +0000

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Inventory-App",
+  base: "/Inventory-App/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
